1d3abeeed3e0e932ac5e873c19dcc39331ca90fe
seki-mei <>
2025-07-31 20:06:56 +0200

    configs

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "lemma.moe";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "lemma.moe";
